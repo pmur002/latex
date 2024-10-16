fa550d58d8c69a9439600dc38858bbfc78c4c8eb
Paul Murrell <paul@stat.auckland.ac.nz>
2024-10-16 17:32:27 +1300

    add objToGrob();  even dummy engine can now draw (if in the wrong place);  dummy engine with {ttx} fontLib draws in the right place

--- R/dvi.R
+++ R/dvi.R
@@ -9,16 +9,43 @@ initTeXstate <- function() {
     state
 }
 
+calculateOffset <- function(x, y, hjust, vjust, state) {
+    ## Create dummy font
+    fonts <- TeXget("fonts", state)
+    fonts[[256]] <- list(file="", index=0, size=0, op=NULL)
+    TeXset("fonts", fonts, state)
+    ## Create dummy glyph grob
+    dummyGlyph <- glyph(x=0, y=0, index=0, fontindex=256, size=0)
+    class(dummyGlyph) <- "LaTeXglyphObj"
+    dummyGrob <- objToGrob(dummyGlyph, x=x, y=y, hjust=hjust, vjust=vjust,
+                           state=state)
+    coords <- grobCoords(dummyGrob)
+    ## Offset is location of dummy glyph grob
+    list(x=coords[[1]]$x[1], y=coords[[1]]$y[1])
+}
+
 makeContent.DVIgrob <- function(x, ...) {
     state <- initTeXstate()
     TeXset("packages", x$packages, state)
     TeXset("fontLib", x$fontLib, state)
     TeXset("engine", x$engine, state)
     
-    ## Generate objs from DVI
+    ## Generate objs from DVI,
+    ## which also establishes metrics of text and any other drawing
     invisible(lapply(x$dvi, DVItoObj, state))
-
-    textGrob("Expect more ...")
+    objList <- TeXget("objList", state)
+    if (length(objList)) {
+        ## Calculate offset for non-text drawing
+        offset <- calculateOffset(x$x, x$y, x$hjust, x$vjust, state)
+        ## Generate grobs from objs
+        grobs <- lapply(objList, objToGrob,
+                        x=x$x, y=x$y, hjust=x$hjust, vjust=x$vjust,
+                        xoffset=offset$x, yoffset=offset$y,
+                        state=state)
+        gTree(children=do.call(gList, grobs), cl="LaTeXgrob")
+    } else {
+        gTree(cl="LaTeXgrob")
+    }
 }
 
 ################################################################################
--- tests/latex.R
+++ tests/latex.R
@@ -3,6 +3,21 @@ library(dvi)
 library(latex)
 
 dvi <- readDVI(system.file("DVI", "test-xe.xdv", package="dvi"))
-               
-## Fall back to dummy engine AND to dummy fontLib
-tools::assertWarning(grid.dvi(dvi))
+
+## Font file paths based on my machine
+if (Sys.getenv("USER") == "pmur002") {
+    
+    ## Fall back to dummy engine AND to dummy fontLib
+    ## (glyph positioning is compromised)
+    tools::assertWarning(grid.dvi(dvi))
+
+    if (require("ttx")) {
+        ## Just fall back to dummy engine
+        ## (glyph positioning should be fine)
+        TTX <- FontLibrary(ttx::ttxGlyphWidth,
+                           ttx::ttxGlyphHeight,
+                           ttx::ttxGlyphBounds)
+        tools::assertWarning(grid.dvi(dvi, fontLib=TTX))
+    }
+    
+}
